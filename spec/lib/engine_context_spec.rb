233dbd4407f5ada16f9de74666184698dfb116d6
odelbos <od@phibox.com>
2023-09-26 13:29:49 +0200

    spec: add test for engine context

---
+++ spec/lib/engine_context_spec.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+RSpec.describe Saphyr::Engine do
+
+  let(:validators) { [] }
+  let(:schema) { double('Schema') }
+  let(:data) { {} }
+  let(:fragment) { '' }
+  let(:path) { '//' }
+  let(:errors) { [] }
+  subject(:context) { Saphyr::Engine::Context.new(validators, schema, data, fragment, path, errors) }
+
+  describe '#initialize' do
+    it 'sets the instance variables correctly' do
+      expect(context.validators).to eq validators
+      expect(context.schema).to eq schema
+      expect(context.data).to eq data
+      expect(context.fragment).to eq fragment
+      expect(context.path).to eq path
+      expect(context.errors).to eq errors
+    end
+  end
+
+  describe '#get_path' do
+    context 'when path is the root' do
+      it 'returns the correct path' do
+        expect(context.get_path('field')).to eq '//field'
+      end
+    end
+
+    context 'when path is not the root' do
+      subject { Saphyr::Engine::Context.new(validators, schema, data, fragment, '//preceding', errors) }
+      it 'returns the correct path' do
+        expect(subject.get_path('field')).to eq '//preceding.field'
+      end
+    end
+  end
+
+  describe '#find_schema' do
+    it 'returns the schema from local validators if found' do
+      validator = double "LocalValidator"
+      allow(validator).to receive(:find_schema).with(:schema_name).and_return schema
+      context.instance_variable_set :@validators, [validator]
+      expect(context.find_schema(:schema_name)).to eq schema
+    end
+
+    it 'returns the schema from global schemas if not found locally' do
+      allow(Saphyr).to receive(:global_schema).with(:schema_name).and_return schema
+      expect(context.find_schema(:schema_name)).to eq schema
+    end
+
+    it 'raises an error if not found' do
+      allow(Saphyr).to receive(:global_schema).with(:schema_name).and_return nil
+      expect { context.find_schema(:schema_name) }.to raise_error Saphyr::Error, 'Cannot find schema name: schema_name'
+    end
+  end
+end
