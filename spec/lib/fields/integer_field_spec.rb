50eb5f910b9e2a80e3e86984764d96c1c354c635
odelbos <od@phibox.com>
2023-09-30 10:53:17 +0200

    spec: refa integer field with shared examples

--- spec/lib/fields/integer_field_spec.rb
+++ spec/lib/fields/integer_field_spec.rb
@@ -25,201 +25,30 @@ RSpec.describe Saphyr::Fields::IntegerField do
   #
 
   describe '#do_validate' do
+    let (:assert_prefix) { 'integer' }
     let(:errors) { [] }
-
     subject { described_class.new }
 
-    context "assert type" do
-      context 'when valid data' do
-        it 'return without error' do
-          subject.send :do_validate, nil, 'ref', 3, errors
-          expect(errors.size).to eq 0
-        end
-      end
-
-      context 'when invalid data' do
-        it 'return an error' do
-          subject.send :do_validate, nil, 'ref', 'err', errors
-          expect(errors.size).to eq 1
-          expect(errors.first[:type]).to eq 'integer:type'
-          expect(errors.first[:msg]).to eq "Expecting type 'Integer', got: String"
-        end
-      end
+    context 'assert base class' do
+      let (:assert_value) { 5 }
+      let (:assert_err_value) { 'err' }
+      it_behaves_like 'assert base class'
     end
 
-    context "assert eq" do
-      subject {
-        d = described_class.new
-        m = d.opts.merge({ eq: 5 })
-        d.instance_variable_set :@opts, m
-        d
-      }
-
-      context 'when valid data' do
-        it 'return without error' do
-          subject.send :do_validate, nil, 'name', 5, errors
-          expect(errors.size).to eq 0
-        end
-      end
-
-      context 'when invalid data' do
-        it 'return an error' do
-          subject.send :do_validate, nil, 'name', 8, errors
-          expect(errors.size).to eq 1
-          expect(errors.first[:type]).to eq 'integer:eq'
-          expect(errors.first[:msg]).to eq "Expecting value to be equals to: 5, got: 8"
-        end
-      end
+    context 'assert base eq' do
+      let (:assert_value) { 5 }
+      it_behaves_like 'assert base eq'
     end
 
-    context "assert numeric gt" do
-      context 'when valid data' do
-        subject {
-          d = described_class.new
-          m = d.opts.merge({ gt: 2 })
-          d.instance_variable_set :@opts, m
-          d
-        }
-        it 'return without error' do
-          subject.send :do_validate, nil, 'name', 5, errors
-          expect(errors.size).to eq 0
-        end
-      end
-
-      context 'when invalid data' do
-        subject {
-          d = described_class.new
-          m = d.opts.merge({ gt: 10 })
-          d.instance_variable_set :@opts, m
-          d
-        }
-        it 'return an error' do
-          subject.send :do_validate, nil, 'name', 5, errors
-          expect(errors.size).to eq 1
-          expect(errors.first[:type]).to eq 'integer:gt'
-          expect(errors.first[:msg]).to eq "Expecting value > 10, got: 5"
-        end
-      end
+    context 'assert numric' do
+      let (:assert_value) { 5 }
+      it_behaves_like 'assert numeric'
     end
 
-    context "assert numeric gte" do
-      context 'when valid data' do
-        subject {
-          d = described_class.new
-          m = d.opts.merge({ gte: 5 })
-          d.instance_variable_set :@opts, m
-          d
-        }
-        it 'return without error' do
-          subject.send :do_validate, nil, 'name', 5, errors
-          expect(errors.size).to eq 0
-        end
-      end
-
-      context 'when invalid data' do
-        subject {
-          d = described_class.new
-          m = d.opts.merge({ gte: 10 })
-          d.instance_variable_set :@opts, m
-          d
-        }
-        it 'return an error' do
-          subject.send :do_validate, nil, 'name', 5, errors
-          expect(errors.size).to eq 1
-          expect(errors.first[:type]).to eq 'integer:gte'
-          expect(errors.first[:msg]).to eq "Expecting value >= 10, got: 5"
-        end
-      end
-    end
-
-    context "assert numeric lt" do
-      context 'when valid data' do
-        subject {
-          d = described_class.new
-          m = d.opts.merge({ lt: 10 })
-          d.instance_variable_set :@opts, m
-          d
-        }
-        it 'return without error' do
-          subject.send :do_validate, nil, 'name', 5, errors
-          expect(errors.size).to eq 0
-        end
-      end
-
-      context 'when invalid data' do
-        subject {
-          d = described_class.new
-          m = d.opts.merge({ lt: 3 })
-          d.instance_variable_set :@opts, m
-          d
-        }
-        it 'return an error' do
-          subject.send :do_validate, nil, 'name', 3, errors
-          expect(errors.size).to eq 1
-          expect(errors.first[:type]).to eq 'integer:lt'
-          expect(errors.first[:msg]).to eq "Expecting value < 3, got: 3"
-        end
-      end
-    end
-
-    context "assert numeric lte" do
-      context 'when valid data' do
-        subject {
-          d = described_class.new
-          m = d.opts.merge({ lte: 5 })
-          d.instance_variable_set :@opts, m
-          d
-        }
-        it 'return without error' do
-          subject.send :do_validate, nil, 'name', 5, errors
-          expect(errors.size).to eq 0
-        end
-      end
-
-      context 'when invalid data' do
-        subject {
-          d = described_class.new
-          m = d.opts.merge({ lte: 3 })
-          d.instance_variable_set :@opts, m
-          d
-        }
-        it 'return an error' do
-          subject.send :do_validate, nil, 'name', 5, errors
-          expect(errors.size).to eq 1
-          expect(errors.first[:type]).to eq 'integer:lte'
-          expect(errors.first[:msg]).to eq "Expecting value <= 3, got: 5"
-        end
-      end
-    end
-
-    context "assert in" do
-      context 'when valid data' do
-        subject {
-          d = described_class.new
-          m = d.opts.merge({ in: [3, 5] })
-          d.instance_variable_set :@opts, m
-          d
-        }
-        it 'return without error' do
-          subject.send :do_validate, nil, 'name', 5, errors
-          expect(errors.size).to eq 0
-        end
-      end
-
-      context 'when invalid data' do
-        subject {
-          d = described_class.new
-          m = d.opts.merge({ in: [3, 5] })
-          d.instance_variable_set :@opts, m
-          d
-        }
-        it 'return an error' do
-          subject.send :do_validate, nil, 'name', 8, errors
-          expect(errors.size).to eq 1
-          expect(errors.first[:type]).to eq 'integer:in'
-          expect(errors.first[:msg]).to eq "Expecting value to be in: [3, 5], got: 8"
-        end
-      end
+    context 'assert base in' do
+      let (:assert_values) { [3, 5, 7] }
+      let (:assert_err_value) { 15 }
+      it_behaves_like 'assert base in'
     end
   end
 end
