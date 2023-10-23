ec4bd79c1a6907ada8a850625831fe3c79cb4025
odelbos <od@phibox.com>
2023-10-23 11:31:11 +0200

    feat: add examples for conditional fields

---
+++ examples/10_conditional_with_methods.rb
@@ -0,0 +1,111 @@
+require_relative '../lib/saphyr'
+require_relative './common'
+
+# ----------------------------------------------
+# Defining validation schema
+# ----------------------------------------------
+class ItemValidator < Saphyr::Validator
+  field :id,    :integer,   gt: 0
+  field :type,  :string,    in: ['post', 'file']
+  field :code,  :string
+
+  conditional :is_file? do
+    field :content,   :string
+    field :author,    :string
+  end
+
+  conditional :is_post? do
+    field :name,  :string,  min: 2
+    field :mime,  :string,  in: ['image/png', 'image/jpg']
+  end
+
+  conditional :code_r3? do
+    field :ref,  :string,  min: 2
+  end
+
+  private
+
+    def is_file?
+      get(:type) == 'file'
+    end
+
+    def is_post?
+      get(:type) == 'post'
+    end
+
+    def code_r3?
+      get(:code) == 'R3'
+    end
+end
+
+
+# ----------------------------------------------
+# Defining some data to validate
+# ----------------------------------------------
+VALID_DATA_1 = {
+  "id" => 145,
+  "type" => "file",
+  "code" => "R3",
+
+  # Condtionals fields: if type == file
+  "content" => "Lipsum ...",
+  "author" => "Lipsum ...",
+
+  # Condtionals fields: if code == R3
+  "ref" => "Lipsum ...",
+}
+
+
+VALID_DATA_2 = {
+  "id" => 145,
+  "type" => "post",
+  "code" => "OP",
+
+  # Condtionals fields: if type == psot
+  "name" => "Lipsum ...",
+  "mime" => "image/png",
+}
+
+
+VALID_DATA_3 = {
+  "id" => 145,
+  "type" => "post",
+  "code" => "R3",
+
+  # Condtionals fields: if type == psot
+  "name" => "Lipsum ...",
+  "mime" => "image/png",
+
+  # Condtionals fields: if code == R3
+  "ref" => "Lipsum ...",
+}
+
+
+ERROR_DATA = {
+  "id" => 145,
+  "type" => "post",
+  "code" => "R3",
+
+  # Condtionals fields: if type == file
+  "content" => "Lipsum ...",
+  "author" => "Lipsum ...",
+
+  # Condtionals fields: if code == R3
+  "ref" => "Lipsum ...",
+}
+
+
+# ----------------------------------------------
+# Validate data
+# ----------------------------------------------
+v = ItemValidator.new
+
+# Example using: v.validate(data)
+#
+validate v, VALID_DATA_1
+
+validate v, VALID_DATA_2
+
+validate v, VALID_DATA_3
+
+validate v, ERROR_DATA
